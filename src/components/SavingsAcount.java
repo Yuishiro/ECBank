7d1a664015f028f98c6fac6ec321b0f93e35b3a2
Yuishiro <elodine.coquelet@epita.fr>
2024-05-17 15:16:38 +0200

    CurrentAccount and Savings Account class

---
+++ src/components/SavingsAcount.java
@@ -0,0 +1,10 @@
+package components;
+
+// 1.2.2 Creation of the CurrentAccount and SavingsAccount
+public class SavingsAcount extends Account{
+
+	public SavingsAcount(String label, Client client) {
+		super(label, client);
+	}
+
+}
